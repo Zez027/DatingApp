3984c45f660b0008d91a6056d8c92c7b313c68d5
jose victor <jsvictor.jvm@gmail.com>
2024-08-27 00:58:45 -0300

    Fim da secao 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { response } from 'express';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -13,23 +14,22 @@ import { User } from '../_models/user';
 export class NavComponent implements OnInit{
   model: any = {};
 
-  constructor(public accountService: AccountService, private router: Router) { }
+  constructor(public accountService: AccountService, private router: Router, 
+  private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-      },
-      error: error => console.log(error)
+      next: _ => this.router.navigateByUrl('/members'),
+      error: error => this.toastr.error(error.error)
     })
     console.log(this.model);
   }
 
   loggout() {
     this.accountService.loggout();
-    this.router.navigate(['/']);
+    this.router.navigateByUrl('/');
   }
 }
